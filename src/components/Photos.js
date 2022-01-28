2bfe6885001a9d4503597b75f5f82ed847aa0112
Verrell <verrell.soriano09@gmail.com>
2022-01-28 12:18:04 +1100

    Added Components Header | Photos | Footer

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
-function Photos() {
 
+import { FaHeart } from "react-icons/fa";
+
+function Photos(props) {
+    console.log(props)
+
+
+    // populate photo data into array state
     const [photos, SetPhotos] = useState([])
 
     const getRandomPhotos = async () => {
-        const res = await axios.get(`https://api.unsplash.com/photos/?client_id=zm6yP5utQrDdjin90JVGm7qie_4wPSZvbmUSpJnWsa0`)
-        console.log(res.data);
-        SetPhotos(res.data)
-        const data = await res.data;
+        try {
+            const res = await axios.get(`https://api.unsplash.com/photos/?client_id=zm6yP5utQrDdjin90JVGm7qie_4wPSZvbmUSpJnWsa0`)
+            const data = await res.data;
+            console.log(res)
+            if (res.status) {
+                SetPhotos(data)
+            }
+        } catch (err) {
+            console.log(err)
+        }
     }
 
     useEffect(() => {
@@ -19,21 +33,33 @@ function Photos() {
     },[]);
 
 
-    function lightboxCustom() {
-        console.log('open swiper')
+    function lightboxCustom(i) {
+        props.triggerModal(i)
+        // console.log(i)
     }
 
   return (
     <ResponsiveMasonry
         columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
     >
-        <Masonry className="photos" gutter='10px' onClick={lightboxCustom}>
+        <Masonry className="photos" gutter='10px'>
             {
                 photos.map((item, i) => {
                     return(
-                        <div className="holder-img" key={i}>
-                            <img className="img"  src={item.urls.raw} alt={item.description}/>
+                        <div className="holder-img" key={i} onClick={() => lightboxCustom(item)}>
+                            <LazyLoadImage 
+                                effect="blur"
+                                className="img"  
+                                src={item.urls.regular} 
+                                alt={item.alt_description}
+                                width="100%"
+                                height="auto"/>
                             {/* <p>{item.alt_description}</p> */}
+
+                            <div className="overlay">
+                                <p className="overlay-name">{item.user.name}</p>
+                                <p className="overlay-heart"> <FaHeart /> {item.user.total_likes}</p>
+                            </div>
                         </div>
                     )
                 })
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,25 +5,55 @@ import Footer from '../components/Footer';
 import Photos from '../components/Photos';
 
 
-// import {
-//   Row,
-//   Col,
-//   Button
-// } from 'react-bootstrap'
+import {
+  Modal,
+  Col,
+  Button
+} from 'react-bootstrap'
 
 
-const Home = () => {    
+const Home = () => {
+  
+  const [show, setShow] = useState(false);
+  const [modalData, setModalData] = useState({})
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleClose = () => setShow(false);
+
+  function handleOpenMenu(i) {
+    show ? setShow(false) : setShow(true)
+    if (i) {
+      setModalData(i)
+      setIsModalOpen(true)
+    }
+    console.log(i)
+  }
 
   return (
-    <main className="home">
-
-      {/* Header component */}
+    <>
       <Header />
+        <main className="home">
 
-      <Photos />
+        <Photos triggerModal={(i) => handleOpenMenu(i)}/>
 
+        </main>
       <Footer />
-    </main>
+      {
+        isModalOpen ?
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Body>
+            <div className="holder-img">
+              <img src={modalData.urls.small} alt={ modalData.description}/>
+            </div>
+            <div className="holder-description">
+              <p className="description">{modalData.description ? modalData.description : 'User did not specify the iamge description'}</p>
+              <p className="user">@{modalData.user.name}</p>
+            </div>
+          </Modal.Body>
+        </Modal> : ''
+      }
+      
+    </>
   );
 }
 
